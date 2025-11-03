4626d7aac7765ea2d198ffde21619e54fa3d950b
Mike <mike@niice.nl>
2025-11-03 10:59:22 +0100

    gsap-heading

---
+++ js/gsap-heading.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  let headings = document.querySelectorAll('[data-split="heading"]')
+  headings.forEach(heading => {
+    SplitText.create(heading, {
+        type: "lines",
+        autoSplit: true,
+        mask: "lines",
+        onSplit(instance) {
+          return gsap.from(instance.lines, {
+            duration: 0.8, 
+            yPercent: 110,
+            stagger: 0.1,
+            ease:"expo.out",
+          });
+        }
+    });    
+  });
+  
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  let headings = document.querySelectorAll('[data-split="heading"]')
+  headings.forEach(heading => {
+    SplitText.create(heading, {
+      type: "lines",
+      autoSplit: true,
+      mask: "lines",
+      onSplit(instance) {
+        return gsap.from(instance.lines, {
+          duration: 0.8, 
+          yPercent: 110,
+          stagger: 0.1,
+          ease:"expo.out",
+          scrollTrigger: {
+            trigger: heading,
+            start: 'top 80%',
+            once: true,
+          }
+        });
+      }
+    });    
+  });
+});
