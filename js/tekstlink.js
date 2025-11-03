c09c48b48708f4ae9bfcba2ad0fb9a5341b7a455
Mike <mike@niice.nl>
2025-11-03 16:12:57 +0100

    changes draw svg because sizing didnt work

--- js/tekstlink.js
+++ js/tekstlink.js
@@ -15,6 +15,9 @@ function initDrawRandomUnderline() {
   function decorateSVG(svgEl) {
     svgEl.setAttribute('class', 'text-draw__box-svg');
     svgEl.setAttribute('preserveAspectRatio', 'none');
+    svgEl.style.width = '100%';
+    svgEl.style.height = 'auto';
+    svgEl.style.display = 'block';
     svgEl.querySelectorAll('path').forEach(path => {
       path.setAttribute('stroke', 'currentColor');
     });
